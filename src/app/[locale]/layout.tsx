3cf3bcd232787a162beddceca45609fe47df350a
cih <kfc_coco@naver.com>
2026-07-28 13:41:15 +0900

    Remove Kiwitaxi search widget

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,6 @@
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 import { LocaleHtmlLang } from "@/components/layout/LocaleHtmlLang";
-import { TopTravelWidget } from "@/components/layout/TopTravelWidget";
 import { EmrldTracker } from "@/components/analytics/EmrldTracker";
 import { ServiceWorkerRegister } from "@/components/pwa/ServiceWorkerRegister";
 import { isProductionRuntime } from "@/lib/runtime";
@@ -28,7 +27,6 @@ export default async function LocaleLayout({
     <>
       <LocaleHtmlLang locale={locale} />
       <Header locale={locale} />
-      <TopTravelWidget />
       <main className="flex-1">{children}</main>
       <Footer locale={locale} />
       {enableThirdParty && <EmrldTracker />}
